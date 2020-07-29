be6e8e3ebba624d91beddac56f19fa8a885c3247
VaibAPP <63972645+vaibhavdagar@users.noreply.github.com>
2020-07-29 12:24:39 +0530

    Update config.php

--- vyaapaar/include/config.php
+++ vyaapaar/include/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','iservign_vaibhav');
-define('DB_PASS' ,'Vaibhav1');
-define('DB_NAME', 'iservign_vyaapaar');
+define('DB_USER','');
+define('DB_PASS' ,'');
+define('DB_NAME', '');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
